f140faaf5ce743ed224824a8bb701639b27548a2
unknown <paolomartinligsay@gmail.com>
2022-01-13 10:13:53 +0800

    added watch, minify scripts for js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,15 +1,6 @@
-(function () {
-  'use strict';
-
-  function module1() {
-    console.log(2);
-  }
-
-  function module2() {
-    console.log(1);
-  }
-
-  module1();
-  module2();
-
-})();
+(function(){'use strict';function module1() {
+  console.log(1);
+}function module2() {
+  console.log(1);
+}module1();
+module2();})();
